84bd0b4413c95a60e7bf57c7e6f046c08b532717
seanaleid <seanaleid@gmail.com>
2019-11-06 17:16:42 -0500

    Adds functional code for GET /:id of schemes

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,16 @@
 const db = require('../dbConfig.js')
 
 module.exports = {
-    find
+    find,
+    findById
 }
 
 function find() {
     return db('schemes')
+};
+
+function findById(id) {
+    return db('schemes')
+        .where({id})
+        .first();
 }
