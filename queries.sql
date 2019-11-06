2e6548c88388a0ca4e18cd031e55364bb951c01a
seanaleid <seanaleid@gmail.com>
2019-11-06 16:29:53 -0500

    Adds sql queries, all are functional and meet specifications

--- queries.sql
+++ queries.sql
@@ -9,6 +9,22 @@ order by c.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+select [Order].Id as OrderId, Shipper.CompanyName 
+from [Order] 
+join Shipper on [Order].ShipVia = Shipper.Id 
+where OrderDate < '2012-08-09' ;
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select p.ProductName as Product, od.Quantity as Quantity
+from [OrderDetail] as od
+join Product as p on od.ProductId = p.id
+where OrderId = 10251
+order by Product;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select o.Id as OrderId, c.CompanyName as CustomerCompanyName, e.LastName as EmployeeLastName
+from [Order] as o
+join Customer as c on o.CustomerId = c.Id
+join Employee as e on o.EmployeeId = e.Id;
